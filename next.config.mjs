878b9d47592995dfd168f40277653819b453c294
Mario André Gonzalez Rodriguez <mandregr@Marios-MacBook-Pro.local>
2024-12-09 20:58:52 -0300

    next config

---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+    output: 'export',
+    trailingSlash: true,
+    basePath: '/roper-s.com', 
+    assetPrefix: '/roper-s.com',
+  }
+  
+  export default nextConfig;
